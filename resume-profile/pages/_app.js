290980ce92bae5a095a449403cf1aee2aa96619f
adhyaya <mansithakur240499@gmail.com>
2021-12-02 22:20:55 +0530

    tailwind basic setup

--- resume-profile/pages/_app.js
+++ resume-profile/pages/_app.js
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css';
+// import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  console.log("🚀 ~ file: _app.js ~ line 8 ~ MyApp ~ pageProps", pageProps)
   return <Component {...pageProps} />
 }
 
