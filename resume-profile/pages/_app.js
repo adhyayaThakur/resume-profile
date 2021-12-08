15d6f2d2a3004ae6e5604fac902f1529baba851f
Vaibhav <vaibhav.mittal1998@gmail.com>
2021-12-08 21:07:38 +0530

    update tailwind config and audit fix

--- resume-profile/pages/_app.js
+++ resume-profile/pages/_app.js
@@ -1,8 +1,6 @@
-import 'tailwindcss/tailwind.css';
-// import '../styles/globals.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  console.log("🚀 ~ file: _app.js ~ line 8 ~ MyApp ~ pageProps", pageProps)
   return <Component {...pageProps} />
 }
 
--- resume-profile/pages/index.js
+++ resume-profile/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="max-w-xs rounded overflow-hidden shadow-lg my-2">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">Next + Tailwind ❤️</div>
-          <p className="text-grey-darker text-base">
+          <p>
             Next and Tailwind CSS are a match made in heaven, check out this article on how
             you can combine these two for your next app.
           </p>
