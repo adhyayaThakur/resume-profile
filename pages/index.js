b50e7f5e2c63091ac6d8891f1d0193bbcb8d0e45
Unicifer <37249809+vaibhavmittal1998@users.noreply.github.com>
2021-12-09 12:22:28 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+
+      <div className="max-w-xs rounded overflow-hidden shadow-lg my-2">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Next + Tailwind ❤️</div>
+          <p>
+            Next and Tailwind CSS are a match made in heaven, check out this article on how
+            you can combine these two for your next app.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
